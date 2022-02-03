86bff1298e985054c7cb2ed52fa258d165b643f1
Pjotr Savitski <pjotr.savitski@gmail.com>
2022-02-03 10:05:21 +0200

    References #83, added a watch task.

--- web/themes/custom/bfd_tlu/gulpfile.js
+++ web/themes/custom/bfd_tlu/gulpfile.js
@@ -3,39 +3,56 @@
  * Provides Gulp configurations and tasks for Bootstrap for Drupal theme.
  */
 'use strict';
-const { src, watch, series, dest } = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
+const { src, watch, series, dest } = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
 
-// Initialise browser-sync
+let browserSyncInitialised = false
+
+// Initialise browser-sync.
 function browserSyncInit(cb) {
+  browserSyncInitialised = true
+
   browserSync.init({
     proxy: 'http://YOUR--DEV-URL.COM'
   })
   cb()
 }
 
-// Reload browser-sync
+// Reload browser-sync.
 function browserSyncReload(cb) {
   browserSync.reload()
   cb()
 }
 
-// Watch files, run sass task and reload browser-sync
+// Watch files, run sass task and reload browser-sync.
 function watchTask() {
-  watch('assets/scss/**/*.scss', series(sassTask, browserSyncReload))
+  const tasks = [sassTask]
+
+  if (browserSyncInitialised) {
+    tasks.push(browserSyncReload)
+  }
+
+  watch('assets/scss/**/*.scss', series(...tasks))
 }
 
 // Compile SASS into CSS & auto-inject into browsers.
 function sassTask() {
-  return src('assets/scss/style.scss')
+  const stream = src('assets/scss/style.scss')
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(dest('assets/css'))
-    .pipe(browserSync.stream())
+
+    if (browserSyncInitialised) {
+      stream
+        .pipe(browserSync.stream())
+    }
+
+    return stream
 }
 
-exports.sass = sassTask
+exports.build = sassTask
+exports.watch = series(sassTask, watchTask)
 exports.serve = series(sassTask, browserSyncInit, watchTask)
 exports.default = exports.serve
